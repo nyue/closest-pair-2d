e55d8f1729832a2caa5b8fca990a20a4b09e1e55
Nicholas Yue <yue.nicholas@gmail.com>
2022-10-31 18:46:27 -0700

    ref: Move OSD face data inside of class rather than global
    Also use int instead of size_t for indices so we can leverage
    -1 as a sentinel

--- Indexing/vector/struct_main.cpp
+++ Indexing/vector/struct_main.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-typedef std::vector<size_t> PointIndicesType;
+typedef std::vector<int> PointIndicesType;
 class OSDFaceData {
 public:
 	OSDFaceData()
@@ -29,6 +29,7 @@ public:
 		assert(_index>=0);
 		return _centroid;
 	}
+	int index() const { return _index;}
 protected:
 	int _index; // mimic OpenSubdiv refiner face indices which is a Vtr::Index of type `int`
 	Imath::Box2f _bbox;
@@ -36,28 +37,32 @@ protected:
 };
 typedef std::vector<OSDFaceData> OSDFaceDataArrayType;
 
-OSDFaceDataArrayType globalPA;
-
 class MyClosestPair {
+	const OSDFaceDataArrayType& _faceData;
+
 	bool compareX(const size_t& p, const size_t& q) {
-		return globalPA[p].centroid().x < globalPA[q].centroid().x;
+		return _faceData[p].centroid().x < _faceData[q].centroid().x;
 	}
 
 	bool compareY(const size_t& p, const size_t& q) {
-		return globalPA[p].centroid().y < globalPA[q].centroid().y;
+		return _faceData[p].centroid().y < _faceData[q].centroid().y;
 	}
 
 public:
 
 	typedef std::pair<std::pair<size_t, size_t>, float> DResult;
 
+	MyClosestPair(const OSDFaceDataArrayType& data)
+	:_faceData(data){
+
+	}
 	DResult closestPair(PointIndicesType& L) {
 		std::sort(L.begin(), L.end(), hboost::bind(&MyClosestPair::compareX, this, _1, _2));
 		PointIndicesType res_points;
 		DResult d, d1, d2;
 		int n = L.size();
 		int mid_point = n / 2;
-		float mid = globalPA[L[mid_point]].centroid()[0];
+		float mid = _faceData[L[mid_point]].centroid()[0];
 		if (n <= 1) {
 			return DResult
 					{std::pair<size_t, size_t>{0, 0}, std::numeric_limits<float>::infinity()};
@@ -78,12 +83,12 @@ public:
 		d = d1.second < d2.second ? d1 : d2;
 
 		for (int i = L1.size() - 1; i >= 0; i--) {
-			if (globalPA[L1[i]].centroid()[0] > mid - d.second) {
+			if (_faceData[L1[i]].centroid()[0] > mid - d.second) {
 				L_strip.push_back(L1[i]);
 			}
 		}
 		for (int i = 0; i < L2.size(); i++) {
-			if (globalPA[L2[i]].centroid()[0] < mid + d.second) {
+			if (_faceData[L2[i]].centroid()[0] < mid + d.second) {
 				L_strip.push_back(L2[i]);
 			}
 		}
@@ -111,7 +116,7 @@ public:
 
 	void print_points(const PointIndicesType& points) {
 		for (size_t p: points) {
-			std::cout << globalPA[p].centroid();
+			std::cout << _faceData[p].centroid();
 		}
 		std::cout << endl;
 	}
@@ -146,7 +151,7 @@ private:
 		int L1_ptr = 0;
 		int L2_ptr = 0;
 		while (L1_ptr < L1.size() and L2_ptr < L2.size()) {
-			if (globalPA[L1[L1_ptr]].centroid()[coor] <= globalPA[L2[L2_ptr]].centroid()[coor]) {
+			if (_faceData[L1[L1_ptr]].centroid()[coor] <= _faceData[L2[L2_ptr]].centroid()[coor]) {
 				res.push_back(L1[L1_ptr++]);
 			} else {
 				res.push_back(L2[L2_ptr++]);
@@ -164,8 +169,8 @@ private:
 		// float xDiff = p[0] - q[0];
 		// float yDiff = p[1] - q[1];
 
-		float xDiff = globalPA[p].centroid()[0] - globalPA[q].centroid()[0];
-		float yDiff = globalPA[p].centroid()[1] - globalPA[q].centroid()[1];
+		float xDiff = _faceData[p].centroid()[0] - _faceData[q].centroid()[0];
+		float yDiff = _faceData[p].centroid()[1] - _faceData[q].centroid()[1];
 
 		return std::pair<std::pair<size_t, size_t>, float>
 		{std::pair<size_t, size_t> {p, q}, sqrt(xDiff * xDiff + yDiff * yDiff)};
@@ -174,53 +179,65 @@ private:
 
 }; // MyClosest Pair
 
-/**
- * Run in terminal to compile:
- $ g++ -c merge_sort.cpp 2d_problem.cpp main.cpp && g++ merge_sort.o 2d_problem.o main.o -o my_program && ./my_program 
- * */
-int main() {
-	int point_num = 100000;
-	PointIndicesType pi(point_num);
+void generate_face_data(OSDFaceDataArrayType& faceData)
+{
+	int point_num = 9;
 	int rangeS = 0;
 	int rangeE = 1;
 	srand(time(0));
 	std::random_device rand_dev;
 	std::mt19937 generator(rand_dev());
 	std::uniform_real_distribution<> distr(rangeS, rangeE);
-	// vector<Imath::V2f> points;
 	clock_t tStart = clock();
 	for (int i = 0; i != point_num; i++) {
-		// vector<float> coor{(float)distr(generator), (float)distr(generator)};
-		// points.push_back(Imath::V2f(coor[0],coor[1]));
-		// globalPA.push_back(Imath::V2f(coor[0],coor[1]));
-		globalPA.push_back(OSDFaceData());
-		globalPA.back().init(i, Imath::Box2f(Imath::V2f((float)distr(generator), (float)distr(generator)),Imath::V2f((float)distr(generator), (float)distr(generator))));
-		pi[i] = i;
+		faceData.push_back(OSDFaceData());
+		faceData.back().init(i, Imath::Box2f(Imath::V2f((float)distr(generator), (float)distr(generator)),Imath::V2f((float)distr(generator), (float)distr(generator))));
 	}
-	// printf("Time taken to initialize: \033[0;32m%fs\033[0m\n\n", (float)(clock() - tStart)/CLOCKS_PER_SEC);
 	std::cout << hboost::format("Time taken to initialize: \033[0;32m%fs\033[0m\n\n") % ((float)(clock() - tStart)/CLOCKS_PER_SEC);
-	// printf("\nThere are \033[1;31m%d\033[0m points, whose coordinates are in the interval \033[1;31m[%d, %d]\033[0m.\n\n", point_num, rangeS, rangeE);
 	std::cout << hboost::format("\nThere are \033[1;31m%d\033[0m points, whose coordinates are in the interval \033[1;31m[%d, %d]\033[0m.\n\n") % point_num % rangeS % rangeE;
-	tStart = clock();
-	MyClosestPair cp;
-	std::cout << hboost::format("\npi.size(BEFORE) = %1%") % pi.size() << std::endl;
-	MyClosestPair::DResult res = cp.closestPair(pi);
-	std::cout << hboost::format("\npi.size(AFTER) = %1%") % pi.size() << std::endl;
-	//printf("The closest pair of points is: \033[0;33m%s\033[0m and \033[0;33m%s\033[0m, with the distance \033[1;31m%f\033[0m.\n\n",
-	//        res.first.first.toString().c_str(), res.first.second.toString().c_str(), res.second);
+}
+
+void get_point_indices_from_face_data(const OSDFaceDataArrayType& faceData,
+		PointIndicesType& o_pi)
+{
+	for (auto item:faceData)
+	{
+		o_pi.push_back(item.index());
+	}
+}
+
+void remove_processed_indices(PointIndicesType& indicesData)
+{
+
+}
+
+/**
+ * Run in terminal to compile:
+ $ g++ -c merge_sort.cpp 2d_problem.cpp main.cpp && g++ merge_sort.o 2d_problem.o main.o -o my_program && ./my_program
+ * */
+int main() {
+	OSDFaceDataArrayType faceData;
+	PointIndicesType indicesData;
+
+	generate_face_data(faceData);
+	get_point_indices_from_face_data(faceData,indicesData);
+	clock_t tStart = clock();
+	MyClosestPair cp(faceData);
+	std::cout << hboost::format("\npi.size(BEFORE) = %1%") % indicesData.size() << std::endl;
+	MyClosestPair::DResult res = cp.closestPair(indicesData);
+	std::cout << hboost::format("\npi.size(AFTER) = %1%") % indicesData.size() << std::endl;
 	std::cout << hboost::format("The closest pair of points is: \033[0;33m%s\033[0m and \033[0;33m%s\033[0m, with the distance \033[1;31m%f\033[0m.\n\n")
         		  % res.first.first % res.first.second % res.second;
-	// printf("Time taken: \033[0;32m%fs\033[0m\n\n", (float)(clock() - tStart)/CLOCKS_PER_SEC);
 	std::cout << hboost::format("Time taken: \033[0;32m%fs\033[0m\n\n") % ((float)(clock() - tStart)/CLOCKS_PER_SEC);
 	std::ofstream file, fres;
 	file.open("plot_struct.txt");
-	for (size_t p: pi) {
-		file << globalPA[p].centroid()[0] << "  " << globalPA[p].centroid()[1] << std::endl;
+	for (size_t p: indicesData) {
+		file << faceData[p].centroid()[0] << "  " << faceData[p].centroid()[1] << std::endl;
 	}
 	file.close();
 	fres.open("res_struct.txt");
-	fres << globalPA[res.first.first].centroid()[0] << "  " << globalPA[res.first.first].centroid()[1] << std::endl;
-	fres << globalPA[res.first.second].centroid()[0] << "  " << globalPA[res.first.second].centroid()[1] << std::endl;
+	fres << faceData[res.first.first].centroid()[0] << "  " << faceData[res.first.first].centroid()[1] << std::endl;
+	fres << faceData[res.first.second].centroid()[0] << "  " << faceData[res.first.second].centroid()[1] << std::endl;
 	fres.close();
 	// HDK's Qt breaks gnuplot
 	// system("gnuplot -e \"plot 'plot.txt' using 1:2 pt 7 ps 1 title 'Points', 'res.txt' using 1:2 pt 7 ps 1 lc rgb 'red' title 'Result'; pause -1\"");
